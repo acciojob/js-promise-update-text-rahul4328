51913234d90360e5b4032b7a005c8e4cab774e52
rahul4328 <125386949+rahul4328@users.noreply.github.com>
2023-05-18 15:47:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+function delayAndResolve(delay, value) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value);
+    }, delay);
+  });
+}
+
+async function updateOutputText() {
+  const outputElement = document.getElementById('output');
+  if (!outputElement) {
+    console.error("Element with ID 'output' not found.");
+    return;
+  }
+
+  try {
+    const result = await delayAndResolve(1000, 'Hello, world!');
+    outputElement.textContent = result;
+    console.log('Text updated successfully!');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+// Call the function to start the process
+updateOutputText();
+
+
